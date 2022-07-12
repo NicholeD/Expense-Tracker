786ae624d8ff3b10607188ccd64814d2ea3c1429
NicholeD <YOUREMAIL@YOUREMAIL.com>
2022-07-12 15:58:06 -0600

    Updated setupEnvironment file.

--- setupEnvironment.sh
+++ setupEnvironment.sh
@@ -10,7 +10,7 @@
 
 # Fill out the following values
 # The path of your repo on github.  Don't but the whole URL, just the part after github.com/
-export UNIT_THREE_REPO_NAME=ata-unit-three-project-$GITHUB_USERNAME
+export UNIT_THREE_REPO_NAME=KenzieAcademy-SoftwareEngineering/ata-unit-three-project-NicholeD
 
 # Do not modify the rest of these unless you have been instructed to do so.
 export UNIT_THREE_PROJECT_NAME=unitproject3
